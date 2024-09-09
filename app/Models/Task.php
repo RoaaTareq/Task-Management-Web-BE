ea5c6b1139c565c07717927eae3433b5a8d053b9
roaaabushaqrah <roaa.abushaqrah@gmail.com>
2024-09-09 17:07:37 +0300

    tas

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,27 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    use HasFactory;
-   
-    
-        protected $fillable = ['title', 'description', 'due_date', 'priority', 'user_id'];
-    
-    
+    protected $fillable = ['title', 'description', 'due_date', 'priority', 'user_id','is_completed'];
 
+    // Many-to-many relationship with Category
     public function categories()
-{
-    return $this->belongsToMany(Category::class);
-}
-// In Task.php
-public function user()
-{
-    return $this->belongsTo(User::class);
-}
+    {
+        return $this->belongsToMany(Category::class, 'category_task');
+    }
 
-public function assignedUsers()
-{
-    return $this->belongsToMany(User::class, 'task_user'); // Assuming you have a pivot table named task_user
-}
-
-
+    // Many-to-many relationship with User
+    public function assignedUsers()
+    {
+        return $this->belongsToMany(User::class, 'task_user');
+    }
 }
